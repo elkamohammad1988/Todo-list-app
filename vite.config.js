479ff4ce714c8dbf7edfdc11b3f0e9ce6b5908d8
elkabouri mohammad <elka.mohammad1988@gmail.com>
2026-04-08 00:49:16 +0200

    Fix vite.config.js configuration

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-
+import path from "path";
 export default defineConfig({
-  base: "/Todo-list-app/",
   plugins: [react(), tailwindcss()],
-});
+  base: "/Todo-list-app/",
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    }
+}}); 
+
+
+
+ 
